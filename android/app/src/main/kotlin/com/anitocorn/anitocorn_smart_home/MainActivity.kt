4a85c7f6eee6d337d25aefc2d6b4676ad481bc04
Thitphavanh <thitphavanh@gmail.com>
2022-11-10 15:06:12 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/anitocorn/anitocorn_smart_home/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.anitocorn.anitocorn_smart_home
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'pages/home_pages.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Smart Home',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
